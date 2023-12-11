6df1bb1b8f788ddfbae892bafcfe752c634210de
hexis <hexis@openBSD.my.domain>
2023-12-10 23:14:40 -0500

    openBSD-dwm

--- dwm/config.h
+++ dwm/config.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -39,9 +39,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "[T]",      tile },    /* first entry is default */
+	{ "<F>",      NULL },    /* no layout function means floating behavior */
+	{ "{M}",      monocle },
 };
 
 /* key definitions */
@@ -57,13 +57,23 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+/*static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+*/
+
+static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL }; 
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *date[]  = { "date-noti", NULL };
+static const char *volup[]  = { "sndioctl", "output.level=+0.02", NULL };
+static const char *voldown[]  = { "sndioctl", "output.level=-0.02", NULL };
+
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = date } },
+	{ MODKEY,                       XK_F6,     spawn,          {.v = volup } },
+	{ MODKEY,                       XK_F5,     spawn,          {.v = voldown } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
